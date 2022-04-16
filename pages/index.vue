24c6e75390d93a2c15a59238584e216fad487469
Kamigami55 <eason@easonchang.com>
2022-04-17 00:16:30 +0800

    feat: add ga4

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'G-SHX7D7M0HY' },
+})
 
 export default Vue.extend({
   name: 'IndexPage',
