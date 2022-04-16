7398da75c0cc02871e8586f8d2c9fe5981e47f70
Kamigami55 <eason@easonchang.com>
2022-04-16 17:26:57 +0800

    feat: update site title & description
    update site title & description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,16 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'eason.ch',
+    title: 'Eason Chang 張楹翔',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Hi! I am Eason. I am a Fullstack Engineer / Maker / Indie-hacker',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
